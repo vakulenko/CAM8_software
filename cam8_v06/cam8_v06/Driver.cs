241f158d1f16c9b275613990d95dadb9921438a0
vakulenko <sergiy.vakulenko@gmail.com>
2015-03-06 02:54:28 +0200

    Bugfix + add test

--- cam8_v06/cam8_v06/Driver.cs
+++ cam8_v06/cam8_v06/Driver.cs
@@ -89,6 +89,7 @@ namespace ASCOM.cam8_v06
 
         public void Dispose()
         {
+            this.CoolerOn = false;
             tecComPort.Dispose();
             tectl.Dispose();
         }
@@ -133,6 +134,7 @@ namespace ASCOM.cam8_v06
                 }
                 else
                 {
+                    this.CoolerOn = false;
                     tecComPort.Close();
                     tectl.LogMessage("Connect Set", "COM close");
                     tecIsConnected = value;
