b9b64e4d95309bfeb962c5e26c3a2ecc130860aa
unknown <sergiy.vakulenko@gmail.com>
2015-11-05 01:32:16 +0200

    Added new versions

---
+++ cam8s_v09/cam8s_v09/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+//
+// TODO - Add your authorship information here
+[assembly: AssemblyTitle("ASCOM.cam8s_v09.Camera")]
+[assembly: AssemblyDescription("ASCOM Camera driver for cam8s_v09")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("The ASCOM Initiative")]
+[assembly: AssemblyProduct("ASCOM Camera driver for cam8s_v09")]
+[assembly: AssemblyCopyright("Copyright © 2015 The ASCOM Initiative")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(true)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9a91378e-56fd-492c-8f91-177ab896a283")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+//
+// TODO - Set your driver's version here
+[assembly: AssemblyVersion("6.0.*")]
+[assembly: AssemblyFileVersion("6.0.0.0")]
